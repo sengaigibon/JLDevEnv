719b271fcd5e51f9f0d922f2f2fc9e953f8fdaf6
Javier Caballero (jknight) <jr.caba@yahoo.com>
2026-03-24 13:36:25 +0100

    feat: add phpunit examples

---
+++ tests/Functional/ExampleATest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ExampleATest extends WebTestCase
+{
+    public function testSomething(): void
+    {
+//        $client = static::createClient();
+//        $crawler = $client->request('GET', '/');
+//
+//        $this->assertResponseIsSuccessful();
+//        $this->assertSelectorTextContains('h1', 'Hello World');
+        $this->assertTrue(true);
+    }
+}
